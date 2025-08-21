5b6c4c14db51816b83fce1d6b265d89d94cc2182
willsather <will@sather.ws>
2025-08-20 22:07:08 -0400

    fix(site): vitest config

--- apps/site/vitest.config.ts
+++ apps/site/vitest.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from "@brands/vitest-config";
+import path from "node:path";
+import { uiConfig } from "@brands/vitest-config/ui";
+import react from "@vitejs/plugin-react-swc";
+import { configDefaults, mergeConfig } from "vitest/config";
 
-export default defineConfig({
+export default mergeConfig(uiConfig, {
+  plugins: [react()],
   test: {
+    ...configDefaults,
+    globals: true,
     environment: "jsdom",
+    exclude: ["node_modules", "e2e"],
     setupFiles: ["./vitest.setup.ts"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
